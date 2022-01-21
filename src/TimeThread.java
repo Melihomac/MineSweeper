a5b2d4148014ec89b885cf0569b91c0b90d62bb0
Kadir Burak Gündüz <kburakgndz@gmail.com>
2022-01-21 20:54:35 +0300

    Files reformatted and fixed warnings

--- src/TimeThread.java
+++ src/TimeThread.java
@@ -1,25 +1,24 @@
 public class TimeThread implements Runnable {
     private Thread t;
-    private Game newGame;
+    private final Game newGame;
 
     TimeThread(Game newGame) {
         this.newGame = newGame;
     }
 
     public void run() {
-        while(true) {
+        while (true) {
             try {
                 Thread.sleep(1000);
-                newGame.timer();
-            }
-            catch (InterruptedException e) {
+                newGame.updateTime();
+            } catch (InterruptedException e) {
                 System.exit(0);
             }
         }
     }
 
     public void start() {
-        if (t==null) {
+        if (t == null) {
             t = new Thread(this);
             t.start();
         }
